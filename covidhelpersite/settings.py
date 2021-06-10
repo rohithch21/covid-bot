a5f1aea6fc68ef699d4bfb143536bbeb8d171cd5
rohithch21 <rohithch16@gmail.com>
2021-06-10 23:57:34 +0530

    deploy with postgres 1

--- covidhelpersite/settings.py
+++ covidhelpersite/settings.py
@@ -74,17 +74,28 @@ WSGI_APPLICATION = 'covidhelpersite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cov19db',
-        'USER': 'root',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'rohit',
         'PASSWORD': 'Ewinmicro!0404',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '5432',
     }
 }
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+    }
+}
+
+import dj_database_url
+db_from_env = dj_database_url(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
